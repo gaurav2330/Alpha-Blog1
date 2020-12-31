07f979123590530502cd6a7c1e88028f39ce18c3
Gaurav Sharma <sgrb26@gmail.com>
2020-12-31 21:56:40 +0530

    Add articl through the browser and check validations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new()
+  end
+
+  def create
+    # This is done for the whitelisting of the keys that you want to pass
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully!"
+      redirect_to @article
+    else
+      render 'new'  
+    end  
+  end
+
 end
